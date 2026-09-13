bfe5855cec3bc7f8a0ed6a6b3ab65db850ac1efb
Ahmed Mohamed <am953297@gmail.com>
2026-09-13 18:20:54 +0300

    updating router again

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
 import Home from './Pages/Home/Home';
 import Blog from './Pages/Blog/Blog';
@@ -8,16 +8,14 @@ import 'tailwindcss';
 
 function App() {
   return (
-    <BrowserRouter basename="/adasa">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="post/:slug" element={<Article />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="post/:slug" element={<Article />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
   );
 }
 
